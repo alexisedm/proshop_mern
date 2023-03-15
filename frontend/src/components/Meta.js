dd66252077f3fbba986c72a1620da3d9549f4feb
“alexisedm” <alexrom98@hotmail.com>
2023-03-15 11:38:28 -0600

    update: fix labels and components to our requirements

--- frontend/src/components/Meta.js
+++ frontend/src/components/Meta.js
@@ -12,9 +12,9 @@ const Meta = ({ title, description, keywords }) => {
 }
 
 Meta.defaultProps = {
-  title: 'Welcome To ProShop',
-  description: 'We sell the best products for cheap',
+  title: 'Bienvenido a GuruShop',
+  description: 'we sell the best products for cheap',
   keywords: 'electronics, buy electronics, cheap electroincs',
 }
 
-export default Meta
+export default Meta;
